9cde7d18d57ce179c88349d93e1ce9c14b8bfb27
tanner0101 <me@tanner.xyz>
2018-05-04 18:41:49 -0400

    add date header to ruby

--- frameworks/ruby/rails/app/controllers/api_controller.rb
+++ frameworks/ruby/rails/app/controllers/api_controller.rb
@@ -1,13 +1,16 @@
 class ApiController < ApplicationController
   def index
+    response['Date'] = Time.now.httpdate
     head 200
   end
 
   def user
+    response['Date'] = Time.now.httpdate
     render plain: params['id']
   end
 
   def register_user
+    response['Date'] = Time.now.httpdate
     head 200
   end
 end
